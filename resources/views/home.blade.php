4f1fddc3277e3b3e6152ee08171224cbd7d5251e
Igor Fernandes <igorunimoc@gmail.com>
2024-05-26 05:48:30 -0300

    Adicionando Bootstrap 5.0 com vite

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('welcome')
+@section('content')
+    
+@endsection
+<h1>sou o igor</h1>
+<div class="container">
+    <button class="btn btn-primary">oi</button>
+    <button class="btn btn-secondary">oi</button>
+    <button class="btn btn-warning">oi</button>
+
+</div>
