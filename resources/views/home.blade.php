491780ccdee7154a280e0e1f20520640ae588bb4
Igor Fernandes <igorunimoc@gmail.com>
2024-07-20 01:36:58 -0300

    renovando tela de login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,46 +2,26 @@
 
 @section('content')
     <title>Home</title>
-    <nav class="navbar bg-gradient-custom">
-        <div class="container">
-            <a class="navbar-brand" href="#" style="margin-left: 95px;">
-                <img src="{{ asset('img/logo.png') }}" alt="Logo da Clínica" width="110" height="70">
-            </a>
-        </div>
-    </nav>
-   
-     <div class="background-image-login">
-        <div class="container bg-login-content">
-            <div class="row justify-content-center">
-                <div class="col-md-4 bg-fundo-img-login">
-                    <img src="{{ asset('img/imagem-login.png') }}" alt="Imagem Login" width="200%">
-                </div>
-                <div class="col-md-6 bg-login">
-                    <form class="text-center txt-branco">
-                        <h1>Login</h1>
-                        <div class="form-group">
-                            <label for="login"></label>
-                            <input type="text" class="form-control" id="login" placeholder="Usuário">
-                        </div>
-                        <div class="form-group">
-                            <label for="senha"></label>
-                            <input type="password" class="form-control" id="senha" placeholder="Senha">
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <div class="form-check mt-4">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                                <label class="form-check-label txt-branco" for="flexCheckDefault">
-                                    Me lembrar
-                                </label>
-                            </div>
-                            <div class="mt-4">
-                                <a class="txt-branco" href="#">Esqueci minha senha</a>
-                            </div>
-                        </div>                    
-                        <button type="submit" class="btn btn-entrar">Entrar</button>
-                    </form>
-                </div>
+    <div class="container-fluid bg-fundo">
+        <div class="row no-gutters ">
+            <div class="col-7 bg-fundo-img-login">
+                <img src="{{ asset('img/img-login.png') }}" alt="Imagem Login" class="img-login">
+            </div>            
+            <div class="col-5 bg-login">
+                <form class="text-center txt-branco form-login">
+                    <h1 class="text-center">Login</h1>
+                    <div class="form-group">
+                        <label for="login" class="text-left">Matrícula/MASP/CPF</label>
+                        <input type="number" class="form-control" id="login" placeholder="Usuário">
+                    </div>
+                    <div class="form-group col-12">
+                        <label for="senha" class="text-left">Senha</label>
+                        <input type="password" class="form-control" id="senha" placeholder="Senha">
+                    </div>
+                    <button type="submit" class="btn btn-entrar">Logar</button>
+                </form>
             </div>
+            
         </div>
-    </div>
+    </div>  
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,14 +4,14 @@
     <meta name="robots" content="noindex">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ env('CLIENT_NAME', 'Clinica') }}</title>
-    <link href="{{ asset('css/clinica.css') }}" rel="stylesheet" type="text/css">
+    <title>{{ env('CLIENT_NAME', 'Ru System') }}</title>
+    <link href="{{ asset('css/ru.css') }}" rel="stylesheet" type="text/css">
     @yield('scripts')
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
     @yield('styles')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link href="{{ asset('css/clinica.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/ru.css') }}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
